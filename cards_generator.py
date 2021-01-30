3fa6acb612167407f4bbbb97d3b24e224b65027e
megahomyak <g.megahomyak@gmail.com>
2021-01-31 01:00:25 +0500

    Made title centralized by default, not when it's long

--- cards_generator.py
+++ cards_generator.py
@@ -103,17 +103,17 @@ def add_gradient_with_text(
     bold_font = FreeTypeFont(bold_font_file_name, size=font_size)
     add_gradient(image, gradient_color, gradient_on_side=gradient_on_side)
     side_padding = image.width // 100
-    description_start_x = (
+    text_start_x = (
         image.width // 7 * 5
     ) if gradient_on_side else side_padding
-    place_for_description = (
+    place_for_text = (
         image.width - (
             side_padding if gradient_on_side else side_padding * 2
-        ) - description_start_x
+        ) - text_start_x
     )
     if symbols_before_description_wrap is None:
         wrapped_description = get_wrapped_text_by_max_width(
-            regular_font, description, place_for_description
+            regular_font, description, place_for_text
         )
     else:
         wrapped_description = textwrap.wrap(
@@ -122,7 +122,7 @@ def add_gradient_with_text(
     wrapped_description = "\n".join(wrapped_description)
     if symbols_before_title_wrap is None:
         wrapped_title = get_wrapped_text_by_max_width(
-            bold_font, title, place_for_description
+            bold_font, title, place_for_text
         )
     else:
         wrapped_title = textwrap.wrap(
@@ -130,7 +130,7 @@ def add_gradient_with_text(
         )
     wrapped_title = "\n".join(wrapped_title)
     title_width, title_height = bold_font.getsize_multiline(wrapped_title)
-    # gap_between_texts = height_of_one_line // 2
+    # gap_between_texts = height_of_one_line // 2  -- This is a pseudocode!
     gap_between_texts = bold_font.getsize(title)[1] // 2
     description_width, description_height = (
         regular_font.getsize_multiline(wrapped_description)
@@ -142,15 +142,22 @@ def add_gradient_with_text(
         title_start_y = round(image.height / 8 * 7) - text_height // 2
         if title_start_y + text_height > image.height - side_padding:
             title_start_y = image.height - side_padding - text_height
-        description_start_x = (image.width - description_width) // 2
-    title_start_x = description_start_x + (description_width - title_width) // 2
+    true_description_start_x = (
+        text_start_x + (place_for_text - description_width) // 2
+    )
+    true_title_start_x = (
+        text_start_x + (place_for_text - title_width) // 2
+    )
     drawer = Draw(image)
     drawer.text(  # Title
-        (title_start_x, title_start_y),
+        (true_title_start_x, title_start_y),
         wrapped_title, fill=text_color, font=bold_font, align="center"
     )
     drawer.multiline_text(  # Description
-        (description_start_x, title_start_y + title_height + gap_between_texts),
+        (
+            true_description_start_x,  # x
+            title_start_y + title_height + gap_between_texts  # y
+        ),
         wrapped_description, fill=text_color, font=regular_font, align="center"
     )
 
